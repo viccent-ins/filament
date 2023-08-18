41ffea57f495619c2f0d9415f0ee87cacae5cc61
viccent-ins <viccent.ins@gmail.com>
2023-08-18 19:35:51 +0700

    implement dashboard widget

---
+++ app/Filament/Widgets/LatestStudents.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Student;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget as BaseWidget;
+use Illuminate\Database\Eloquent\Builder;
+
+class LatestStudents extends BaseWidget
+{
+    protected static ?int $sort = 2;
+    protected int | string | array $columnSpan = 'full';
+
+//    public function table(Table $table): Table
+//    {
+//        return $table
+//            ->query(
+//                // ...
+//            )
+//            ->columns([
+//                // ...
+//            ]);
+//    }
+    protected function getTableQuery(): Builder
+    {
+        return Student::query()
+            ->latest()
+            ->take(5);
+    }
+    protected function getTableColumns(): array
+    {
+        return [
+            TextColumn::make('name')
+                ->toggleable()
+                ->searchable()
+                ->sortable(),
+            TextColumn::make('email')
+                ->toggleable()
+                ->searchable()
+                ->sortable(),
+            TextColumn::make('phone_number')
+                ->toggleable(),
+            TextColumn::make('class.name')
+                ->sortable(),
+            TextColumn::make('section.name')
+                ->sortable()
+                ->searchable(),
+            TextColumn::make('address')
+                ->wrap()
+                ->searchable()
+                ->toggleable()
+        ];
+    }
+    protected function isTablePaginationEnabled(): bool
+    {
+        return false;
+    }
+}
---
+++ app/Filament/Widgets/StatesOveriew.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Classes;
+use App\Models\Section;
+use App\Models\Student;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class StatesOveriew extends BaseWidget
+{
+    protected static ?int $sort = 1;
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('Total Student', Student::count()),
+            Stat::make('Total Classes', Classes::count()),
+            Stat::make('Total Section', Section::count()),
+        ];
+    }
+}
